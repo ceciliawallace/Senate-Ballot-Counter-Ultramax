7d0b2e1955d21bbc01ae7ac2c4c967247b83e85b
Eliana Grosof <elig121@gmail.com>
2019-06-25 18:58:30 -0400

    added some description

--- Ballot.py
+++ Ballot.py
@@ -1,3 +1,9 @@
-#this will be the ballot class
-#checking if push from pycharm works rn
-#checking to push from command line
+'''
+This is the Ballot class.
+This contains the information for each Ballot - which includes:
+    - preferences, a stack (Python list treated as a stack) containing a person's preferences
+    - topChoice, a string of the ballot's current first element
+    - TNumber
+    - timeStamp, a record of the time the ballot was submitted
+Each Ballot which is read in from a spreadsheet that is generated from a Google Form.
+'''
