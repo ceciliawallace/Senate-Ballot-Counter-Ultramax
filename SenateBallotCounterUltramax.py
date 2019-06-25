8d06482003c336071d1c85f0329bbca7c1d81b31
Cecilia <cwallace@oberlin.edu>
2019-06-25 19:27:46 -0400

    #pushing fleshed out ballot class

--- SenateBallotCounterUltramax.py
+++ SenateBallotCounterUltramax.py
@@ -1,1 +1,37 @@
-#comment hi
+'''
+This is the main doc.
+'''
+
+
+allBallots = []
+
+scoreCard = {} #name:numVotes  keeps track of candidates and how many votes they have
+
+
+
+def intakeSpreadsheet():
+
+def checkDoubleBallots():
+
+def redistributeSurplus(candidateName):
+
+def eliminate(candidateName):
+    #will call redistributeEliminated() function
+
+def redistributeEliminated(candidateName):
+
+
+def calculateThreshold(numVotes, numCandidates):
+    threshold = numVotes/numCandidates
+    threshold = int(threshold)
+    return threshold
+
+
+def main():
+
+
+main()
+
+
+
+#need something to execute main here??
