729cdd7991d7a8e656bdb1fa5e6c72aba4b3edd3
ceciliawallace <cwallace@oberlin.edu>
2019-06-25 19:03:50 -0400

    duplicate files after some weird git stuff

--- Ballot.py
+++ Ballot.py
@@ -7,3 +7,19 @@ This contains the information for each Ballot - which includes:
     - timeStamp, a record of the time the ballot was submitted
 Each Ballot which is read in from a spreadsheet that is generated from a Google Form.
 '''
+
+class Ballot:
+    def __init__(self, timeStamp, tNumber):
+        self.timeStamp = timeStamp
+        self.tNumber = tNumber
+        self.preferences = [] # this will contain prefered candidates in order (first pref to last)
+        self.topChoice = ""
+
+    def changeTopChoice(self, newTopChoice):
+        self.changeTopChoice = newTopChoice
+
+    def popPreference(self):
+        if self.preferences not empty:
+
+        self.preferences.
+
